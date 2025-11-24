c64973d016fba980f206eeb8bcccf00d2db4a5ed
muhilmu-ctrl <mmuruga2@charlotte.edu>
2025-11-24 18:21:00 -0500

    Update Introductions.jsx

--- src/Introductions.jsx
+++ src/Introductions.jsx
@@ -1,12 +1,26 @@
-import Header from "./Header";
-import Footer from "./Footer";
+import { useState,useEffect } from "react"
 
 export default function Introductions(){
-    return (
+    const [introData,setIntroData] = useState([]);
+    const [error,setError] = useState(null);
+    useEffect (() =>{
+        fetch("https://dvonb.xyz/api/2025-fall/itis-3135/students?full=1")
+        .then((response) =>{
+            if(response.ok){
+                return response.json();
+            }else{
+                throw new Error(response.status);
+            }
+        })
+        .then((data) => setIntroData(data))
+        .catch((error) => setError(error.message));
+    }, []);
+    return(
         <>
-        <Header/>
-        <h2>Everybody's Introductions</h2>
-        <Footer/>
+        <h2>intros</h2>
+        <p>{JSON.stringify(introData)}</p>
+        {error == null ? "" : <p>Error Code: {error}</p>}
         </>
-    );
+    )
+
 }
