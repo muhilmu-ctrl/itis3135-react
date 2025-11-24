b3c434c0815d6b73ef5da56bbea91946e8e014ec
muhilmu-ctrl <mmuruga2@charlotte.edu>
2025-11-24 18:55:42 -0500

    Update Introductions.jsx

--- src/Introductions.jsx
+++ src/Introductions.jsx
@@ -18,16 +18,44 @@ export default function Introductions(){
     return(
         <>
         <h2>intros</h2>
-        <p>{JSON.stringify(introData)}</p>
+        {/* <p>{JSON.stringify(introData)}</p> */}
         {error == null ? "" : <p>Error Code: {error}</p>}
         {
-            introData.map((data,index) => {
+            introData.map((data,index) => 
                 <article key={index}>
-                    <h3>{data}</h3>
+                    <h3>
+                        {data.name.first} {" "}
+                        {data.name.middleInitial ? data.name.middleInitial + " " : ""} {" "}
+                        {data.name.preferred ? data.name.preferred + " " : ""} {" "}
+                        {data.name.last} {" "}
+                        {data.divider} {" "}
+                        {data.mascot}
+                    </h3>
+                    <figure>
+                        <img src={"https://dvonb.xyz" + data.media.src} alt="#"/>
+                    </figure>
+                    <hr/>
+                    <ul>
+                        <li><strong>Personal Background: </strong>{data.backgrounds.personal}</li>
+                        <li><strong>Professional Background:</strong>{data.backgrounds.professional}</li>
+                        <li><strong>Academic Background: </strong>{data.backgrounds.academic}</li>
+                        <li><strong>Background in this subject: </strong>{data.backgrounds.subject}</li>
+                        <li><strong>Primary Computer Platform: </strong></li>
+                        <li><strong>Courses I am Taking & Why:</strong></li>
+                        <li>
+                        <ul>
+                            <li><strong></strong></li>
+                            <li><strong></strong></li>
+                            <li><strong></strong></li>
+                            <li><strong></strong></li>
+                            <li><strong></strong></li>
+                        </ul>
+                        </li>
+                    </ul>
                 </article>
-            })
+            )
         }
         </>
-    )
+    );
 
 }
